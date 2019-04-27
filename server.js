da2d43857295654523a2417faedfe35c9acb6547
Jorge Ramos <jorginhodeveloper@gmail.com>
2019-04-27 16:05:46 -0300

    Add files to lesson 09

--- server.js
+++ server.js
@@ -12,17 +12,7 @@ mongoose.connect(
 );
 requireDir('./src/models');
 
-const Product = mongoose.model('Product');
-
-// primeira rota
-app.get('/', (req, res) => {
-  Product.create({
-    title: 'React Native',
-    description: 'Build native apps with React',
-    url: 'https://github.com/facebook/react-native'
-  });
-
-  return res.send('Hello Rocketseat!');
-});
+// rotas
+app.use('/api', require('./src/routes'));
 
 app.listen(3001);
