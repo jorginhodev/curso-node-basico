9062dfeeb55a43e549d090fb651858ccc0f3a02d
Jorge Ramos <jorginhodeveloper@gmail.com>
2019-04-27 15:51:43 -0300

    Add files to lesson 08

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 // iniciando o app
 const app = express();
 
 // iniciando o DB
-mongoose.connect('mongodb://localhost:27017/nodeapirs', { useNewUrlParser: true });
+mongoose.connect(
+  'mongodb://localhost:27017/nodeapirs',
+  { useNewUrlParser: true }
+);
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
 
 // primeira rota
 app.get('/', (req, res) => {
-  res.send('Hello Rocketseat!');
+  Product.create({
+    title: 'React Native',
+    description: 'Build native apps with React',
+    url: 'https://github.com/facebook/react-native'
+  });
+
+  return res.send('Hello Rocketseat!');
 });
 
 app.listen(3001);
