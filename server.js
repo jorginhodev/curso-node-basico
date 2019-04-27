8f9838078db4b759f8d1e74ef9e3ad9730c350fd
Jorge Ramos <jorginhodeveloper@gmail.com>
2019-04-27 12:26:04 -0300

    Add files to lesson 07

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+// iniciando o app
 const app = express();
 
+// iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapirs', { useNewUrlParser: true });
+
+// primeira rota
 app.get('/', (req, res) => {
   res.send('Hello Rocketseat!');
 });
